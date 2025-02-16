9f355b111c6bb2226b2b334e72071dacdea61bc3
yansoriano <anduvahatovb@gmail.com>
2025-02-16 22:40:39 +0600

    Added Driver bean with properties and constructor. Configured as a Spring Bean and linked with Car.

---
+++ src/main/java/org/example/driver.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class driver {
+    public void drive() {System.out.println("Driver is running ...");}
+}
